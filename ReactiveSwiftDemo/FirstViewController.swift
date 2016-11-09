92fbedfe72acf627a402de9edbff1ecd7223c8cc
Paopao sa <paopaosa@qq.com>
2016-11-09 19:11:22 +0800

    ReactiveSwift 1.0.0-alpha.3. use textField.reactive.continuousTextValues

--- ReactiveSwiftDemo/FirstViewController.swift
+++ ReactiveSwiftDemo/FirstViewController.swift
@@ -14,12 +14,44 @@ import ReactiveSwift
 class FirstViewController: UIViewController {
     @IBOutlet weak var name:UITextField!
 
+    let property = MutableProperty(0)
+    let tapRec = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.name.rac_textSignal().skip(1).subscribeNext { (next) in
-            print("what is \(next)")
+
+//        self.name.rac_textSignal().skip(1).subscribeNext { (next) in
+//            print("what is \(next)")
+//        }
+        self.name.reactive.continuousTextValues
+            .observe(on: UIScheduler())
+            .observe { event in
+                switch event {
+                case let .value(results):
+                    print("name input is \(results!)")
+                case let .failed(error):
+                    print("name input error \(error)")
+                case .completed, .interrupted:
+                    break
+                }
         }
+        
+        self.view.addGestureRecognizer(tapRec)
+        tapRec.reactive.stateChanged
+        .observe { [unowned self](event) in
+            switch event {
+            case let .value(results):
+                print("tap is \(results)")
+                self.name.resignFirstResponder()
+            case let .failed(error):
+                print("tap failed \(error)")
+            case .completed, .interrupted:
+                break
+            }
+        }
+//        self.property <~ self.name.reactive.continuousTextValues
+
     }
 
     override func didReceiveMemoryWarning() {
